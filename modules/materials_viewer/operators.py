44f202f464dee1481a6cbd2f9062dafbdc68eda0
Zenker <zenkerart@gmail.com>
2022-03-03 15:02:15 +0300

    Fix

--- modules/materials_viewer/operators.py
+++ modules/materials_viewer/operators.py
@@ -35,7 +35,9 @@ class FLUIDP_OT_create_curve(bpy.types.Operator):
     material_name: bpy.props.StringProperty(options={'HIDDEN'})
 
     def execute(self, context: bpy.types.Context):
-        bpy.ops.object.mode_set(mode='OBJECT')
+        if context.active_object is not None:
+            bpy.ops.object.mode_set(mode='OBJECT')
+
         if isinstance(ActiveWidget.data, MaterialsViewerBlendItem):
             d = ActiveWidget.data
             obj = d.load()
